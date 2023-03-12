6d40baf46e7da0ad2d350df7855c69af73fde1e8
davidevilla07 <100322681+davidevilla07@users.noreply.github.com>
2023-03-11 21:25:53 -0500

    Add files via upload

---
+++ Assignmen2/src/com/Assignment2/Staff/CalculateSalary.java
@@ -0,0 +1,17 @@
+package com.Assignment2.Staff;
+// Group Members
+// David Samuels - 2101012913
+// Althea Johnson Roach - 2101012491
+// Kemoy Gallimore - 2017002408
+// Glen Brown - 2101012479
+// Darwin Dallas - 2101011317
+public abstract class CalculateSalary {
+
+    public CalculateSalary()
+    {
+
+    }
+
+    public abstract double Salary();
+
+}
